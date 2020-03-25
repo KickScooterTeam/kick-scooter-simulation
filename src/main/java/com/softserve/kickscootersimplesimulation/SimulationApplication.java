ae5a058166192b933a26834a4a08e21e78a60329
foxrev <alex.lisitsa7@gmail.com>
2020-03-25 22:57:15 +0200

    Hotfix, enable scheduling

--- src/main/java/com/softserve/kickscootersimplesimulation/SimulationApplication.java
+++ src/main/java/com/softserve/kickscootersimplesimulation/SimulationApplication.java
@@ -2,8 +2,10 @@ package com.softserve.kickscootersimplesimulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SimulationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimulationApplication.class, args);
